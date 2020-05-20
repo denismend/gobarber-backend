6ec2511ea450892c37a16008502f83a1f84726ee
denismend <denisladeira1@gmail.com>
2020-05-19 23:08:19 -0300

    tests

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const authenticateUser = new AuthenticateUserService(fakeUserRepository);
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+});
