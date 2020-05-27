b1f5389dffae2949771e2e94090c060add33c260
denismend <denisladeira1@gmail.com>
2020-05-27 00:00:57 -0300

    update profile and tests

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -109,4 +109,14 @@ describe('UpdateProfile', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should not be able to update the profile from non existing user', async () => {
+    await expect(
+      updateProfileService.execute({
+        user_id: 'non-existing-user-id',
+        name: 'test',
+        email: 'test',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
