5c17a7b4fe113b7a812244f406097d285e59933e
denismend <denisladeira1@gmail.com>
2020-06-04 22:35:01 -0300

    upload file in amazon s3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 // localhost:3333/appointments
 usersRouter.post(
